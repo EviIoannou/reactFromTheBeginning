eb64bd622b33932e7ae95aff7db6d49f5c0bda87
Evangelia Ioannou <evievi@outlook.com>
2021-07-18 11:13:06 +0200

    Start with react router

--- router-101/src/Help.js
+++ router-101/src/Help.js
@@ -1,23 +1,24 @@
-// const Help = ()=><h1>Help</h1>
+//const Help = () => <h1>Help</h1>
+
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
-const HelpCustomer = ()=><h1>Help Customer</h1>
-const HelpHost = ()=><h1>Help Host</h1>
+const HelpCustomer = () => <h1>Help customer</h1>
+const HelpHost = () => <h1>Help host</h1>
 
-function Help(props){
-    console.log(props.match);
+function Help(props) {
     return(
         <div>
             <div>
-                <Link to="/help/customer">I am a customer</Link> | 
-                <Link to="/help/host">I am a host</Link>
-            </div>
+                <Link to="/help/customer">I am a customer</Link> |
+                <Link to="/help/host">I am a host</Link>  
+            </div>    
             <p>An image goes here</p>
-            <Route path="/help/customer" component={HelpCustomer} />
-            <Route path="/help/host" component={HelpHost} />
-            <h3>Footer for Help</h3>
+            <Route path="/help/customer" component={HelpCustomer}/>
+            <Route path="/help/host" component={HelpHost}/>
+            <h3>Footer for help</h3>     
         </div>
+
     )
 }
 
--- router-101/src/NavBar/NavBar.js
+++ router-101/src/NavBar/NavBar.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './NavBar.css';
 
-function NavBar(props){
-    // console.log(props);
+function NavBar(props) {
     return(
         <nav className="black">
             <div className="nav-wrapper">
-            <NavLink exact to="/" className="brand-logo">AirBnB</NavLink>
+            <NavLink exact to="/" className="brand-logo">AirBnb</NavLink>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><NavLink to="/host">Become a host</NavLink></li>
                 <li><NavLink to="/help">Help</NavLink></li>
                 <li><NavLink to="/signup">Sign up</NavLink></li>
-                <li><NavLink to="/login">Log in</NavLink></li>
+                <li><NavLink to="/login">Login</NavLink></li>
             </ul>
             </div>
         </nav>
-            
     )
 }
 
